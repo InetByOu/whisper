647cf6df60705f0ac7a481643f73b32dd79c6f85
InetByOu <edisaedi.id@gmail.com>
2025-12-26 07:34:05 +0700

    Delete server directory

--- server/crypto.py
+++
@@ -1,8 +0,0 @@
-KEY = b"whisper-udt-final-key"
-
-def crypt(data, seq):
-    key = KEY + seq.to_bytes(4, 'big')
-    out = bytearray(len(data))
-    for i, b in enumerate(data):
-        out[i] = b ^ key[i % len(key)]
-    return bytes(out)
--- server/router.py
+++
@@ -1,5 +0,0 @@
-def ip_dst(pkt):
-    return ".".join(map(str, pkt[16:20]))
-
-def ip_src(pkt):
-    return ".".join(map(str, pkt[12:16]))
